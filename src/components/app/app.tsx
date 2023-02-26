7968d740baaa92c2581eed3d99a882cc0bd1281b
sbldor <sydor.a@yandex.ru>
2023-02-27 00:09:17 +0300

    fonts

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
+import style from "./app.module.css"
 
 const App: FC = () => {
   return (
     <div className="div">
-
+      <p className={style.ppp}>привет птица</p>
     </div>
   )
 }
