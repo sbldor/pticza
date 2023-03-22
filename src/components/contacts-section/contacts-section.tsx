a63072c8e30c583974c3e4a2a7bc31c216144074
sbldor <sydor.a@yandex.ru>
2023-03-23 01:39:54 +0300

    form

--- src/components/contacts-section/contacts-section.tsx
+++ src/components/contacts-section/contacts-section.tsx
@@ -1,26 +1,50 @@
-import { MouseEvent } from 'react';
+import { MouseEvent, useState } from 'react';
 import style from './contacts-section.module.css';
 import Button from '../button/button';
 
 function ContactsSection() {
+  const [optionVal, setOptionVal] = useState<string>('Тариф');
+
   const submit = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     return console.log(submit);
   };
 
+  const handleChange = (e: { target: { value: string } }) => {
+    setOptionVal(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const styleSelect =
+    optionVal === 'Тариф'
+      ? `${style.select} ${style.select_def}`
+      : style.select;
+
   return (
     <section className={style.section}>
-      <h2>КОНТАКТЫ</h2>
+      <h2 className={style.title}>КОНТАКТЫ</h2>
       <div className={style.cont_flex}>
         <div className={style.cont_form}>
+          <p className={style.subtitle}>Хочешь съемку? Скорее бронируй!</p>
           <form className={style.form}>
-            <input type="text" className={style.input} />
+            <input type="text" placeholder="Имя" className={style.input} />
             <input
               type="tel"
+              placeholder="Номер телефона"
               pattern="\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}"
               className={style.input}
             />
-            <select value="vip" name="rate" className={style.select}>
+            <select
+              value={optionVal}
+              onChange={handleChange}
+              name="rate"
+              className={styleSelect}
+            >
+              {optionVal === 'Тариф' && (
+                <option className={style.option} value="Тариф">
+                  Тариф
+                </option>
+              )}
               <option className={style.option} value="lite">
                 LITE
               </option>
@@ -32,7 +56,7 @@ function ContactsSection() {
               </option>
             </select>
             <div className={style.button}>
-              <Button onClick={submit} />
+              <Button onClick={submit} styles="vip" />
             </div>
           </form>
         </div>
